4d6f4de23c422dd6c56a3797f947ec883ccac74f
cydn95 <devstar.sky20@gmail.com>
2022-11-09 23:09:27 +0530

    Update PrintButton.js

--- components/ui/PrintButton.js
+++ components/ui/PrintButton.js
@@ -10,7 +10,7 @@ const PrintButton = () => {
 
   return(
     <>
-      <div className="printi flex flex-col items-center">
+      <div className="printi flex flex-col items-center md:visible">
         <button onClick={print} className="bg-blue-500 text-white font-bold rounded md:p-2 md:my-2">Print</button>
       </div>
     </>
