6bb65c0b1d611a9108f4a34661017a36cb20c556
cydn95 <devstar.sky20@gmail.com>
2023-03-17 06:40:18 +0530

    updated

--- components/Home/HomeBanner.js
+++ components/Home/HomeBanner.js
@@ -20,7 +20,7 @@ const HomeBanner = ({ closeBanner }) => {
                             </Link>
                         </a>
                     </div>
-                    <Image src='/logo.png' href="https://jntuhresultsweb.vercel.app/" className="w-[60px]  sm:w-[120px]" alt="" width={120} height={60} />
+                    <Image src='/logo.png' href="https://jntuhresultsweb.vercel.app/" className="w-[60px]  sm:w-[120px]" alt="" width={80} height={60} />
                     <small>Get your Results with your CGPA in a second</small>
                     <br />
                     <br />
