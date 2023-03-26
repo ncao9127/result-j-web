91c3a5f3b49a3cd5f382d836e5049f9f78bc4d74
cydn95 <devstar.sky20@gmail.com>
2023-03-26 20:35:32 +0530

    Update Header.js

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -27,7 +27,7 @@ const Header = () => {
         <meta
           property='og:image'
           content={
-            'https://github.com/khaja-moiz/ResultsJntuh/blob/main/public/HomeMain.jpg' 
+            'https://raw.githubusercontent.com/khaja-moiz/ResultsJntuh/main/public/HomeMain.jpg' 
           }
         />
 
