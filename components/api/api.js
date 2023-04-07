5cfb98fbdf22b02cf0b2636355b60e55753f45f7
cydn95 <devstar.sky20@gmail.com>
2023-04-08 05:14:33 +0530

    Update api.js

--- components/api/api.js
+++ components/api/api.js
@@ -1,5 +1,5 @@
 
-const url="https://resultsjntuh.up.railway.app" 
+const url="https://resultsjntuhv1.up.railway.app" 
 const urll="http://127.0.0.1:8000/"
 const urlr="https://jntuhresults.herokuapp.com"
 const urlrr="https://jntuhresults.up.railway.app"
