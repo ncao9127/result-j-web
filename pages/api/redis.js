424e10633cd374d566a607a569f729cf2d7c51d3
cydn95 <devstar.sky20@gmail.com>
2023-08-22 19:53:56 +0530

    revert

--- pages/api/redis.js
+++ pages/api/redis.js
@@ -5,9 +5,9 @@ let redis = new Redis(process.env.REDIS_URL);
 export default async function handler(req, res) {
 
   // Set CORS headers to allow requests from any origin
-  // res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000, https://resultsjntuh.vercel.app, https://resultsjntuh.netlify.app');
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000, https://resultsjntuh.vercel.app, https://resultsjntuh.netlify.app');
 
   // Optionally, you can set other CORS headers if needed
   // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
