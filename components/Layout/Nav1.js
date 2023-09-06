fc39344d7d79588bfccc091ffa0b6c9ee8b8aa0e
cydn95 <devstar.sky20@gmail.com>
2023-09-06 22:48:10 +0530

    updated

--- components/Layout/Nav1.js
+++ components/Layout/Nav1.js
@@ -28,7 +28,7 @@ function SideNavbar() {
                 <div className="p-6 w-1/2 h-screen bg-white z-20 fixed top-0 -left-96 lg:left-0 lg:w-60  peer-focus:left-0 peer:transition ease-out delay-150 duration-200 overflow-y-auto">
                     <div className="flex flex-col justify-start item-center">
 
-                        <h1 className="flex items-center gap-3 text-base text-center cursor-pointer font-bold text-blue-900 border-b border-gray-100 pb-4 w-full">
+                        <h1 className="flex items-center gap-3 ml-2 text-base text-center cursor-pointer font-bold text-blue-900 border-b border-gray-100 pb-4 w-full">
                             <Link href="/">
                                 JNTUH RESULTS
                             </Link>
